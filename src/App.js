0583ad1c9dba1c63d515cbac8990bb0b59961216
Daniel PC <daniel.barczynski@onet.pl>
2022-12-31 16:32:45 +0100

    fetching weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+    const [weather, setWeather] = useState([]);
+    const API_URL = "https://localhost:7000/weatherforecast";
+    const fetchWeather = () => {
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(data => setWeather(JSON.stringify(data)));
+    }
 
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={fetchWeather}>Fetch weather</button>
+                <div>
+                    {weather}
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
